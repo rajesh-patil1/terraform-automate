e3dc27bafc4e4b036bf252acfd76e70e771485db
rajesh-patil1 <115767329+rajesh-patil1@users.noreply.github.com>
2026-09-01 10:12:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
